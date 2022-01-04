3b8ec9e2cc64e53e28306be43d49767296b0d93d
dezren39 <dsp@developing.today>
2022-01-04 08:51:53 -0600

    add trunk

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-struct CPU {
+struct Cpu {
     registers: [u8; 16],
     memory: [u8; 4096],
     position_in_memory: usize,
     stack: [u16; 16],
     stack_pointer: usize,
+}
+fn main() {
+    let _cpu = Cpu {
+        registers: [0; 16],
+        memory: [0; 4096],
+        position_in_memory: 0,
+        stack: [0; 16],
+        stack_pointer: 0,
+    };
 }
