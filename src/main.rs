c9fb6ec6ad9628a24fa863d18ce360ea767125b4
dezren39 <dsp@developing.today>
2022-01-06 23:22:27 -0600

    last time

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,14 @@ struct Cpu {
     stack_pointer: usize,
 }
 fn main() {
-    let _cpu = Cpu {
+    let _cpu = &mut Cpu {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
         stack: [0; 16],
         stack_pointer: 0,
     };
+    _cpu.registers[0] = 1;
+    _cpu.registers[1] = 2;
+    _cpu.memory[0] = 0x01;
 }
