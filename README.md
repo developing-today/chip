1e25fad5c7a7fd7b2c0926722a36ded4dfafe45d
dezren39 <dsp@developing.today>
2022-01-08 18:06:27 -0600

    improve

---
+++ README.md
@@ -0,0 +1,1 @@
+https://github.com/rust-lang/rfcs/blob/master/text/1574-more-api-documentation-conventions.md#appendix-a-full-conventions-text
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
-struct Cpu {
+struct CPU {
     registers: [u8; 16],
     memory: [u8; 4096],
     position_in_memory: usize,
     stack: [u16; 16],
     stack_pointer: usize,
 }
+
+impl CPU {
+    fn read_opcode(&self) -> u16 {
+        7
+    }
+}
+
 fn main() {
-    let _cpu = &mut Cpu {
+    let _cpu = &mut CPU {
         registers: [0; 16],
         memory: [0; 4096],
         position_in_memory: 0,
@@ -14,6 +21,6 @@ fn main() {
         stack_pointer: 0,
     };
     _cpu.registers[0] = 1;
-    _cpu.registers[1] = 2;
+    _cpu.registers[215] = 2;
     _cpu.memory[0] = 0x01;
 }
